ecca62f6db7c25201806e16bd41902db64d5c12a
NikitochkaHrum <ponkin.nikitka@mail.ru>
2023-04-10 18:49:47 +0500

    Добавьте файлы проекта.

---
+++ WpfAppN/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfAppN
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Timer Timer = new Timer(1000);
+        public MainWindow()
+        {
+            InitializeComponent();
+            Timer.Elapsed += TimerElapsed;
+            Timer.Enabled = true;
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            var time = DateTime.Now;
+            Dispatcher.Invoke(
+                DispatcherPriority.Normal,
+                (Action)(() =>
+                {
+                    // Положение секундной стрелки
+                    RotateSecond.Angle = 6 * (time.Second);
+
+                    // Положение минутной стрелки
+                    RotateMinute.Angle = 6 * time.Minute + RotateSecond.Angle / 60;
+
+                    // Положение часовой стрелки
+                    RotateHour.Angle =
+                        30 * (time.Hour % 12) + RotateMinute.Angle / 60;
+                }));
+        }
+    }
+}
